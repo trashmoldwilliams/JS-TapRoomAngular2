40e45452b15d2e83f61ae198c1407741299e7949
lawton browning and will johnson <lawton+will@>
2016-03-23 10:47:51 -0700

    refactored classes

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,37 +1,6 @@
 import { Component, EventEmitter } from 'angular2/core';
+import { KegListComponent } from './keg-list.component';
 import { Keg } from './keg.model';
-
-
-@Component({
-  selector: 'keg-list',
-  inputs: ['kegList'],
-  outputs: ['onKegSelect'],
-  template: `
-      <div *ngFor="#keg of kegList" (click)="kegClicked(keg)"
-      [class.selected]='keg === selectedKeg'>
-        <h3>{{ keg.name }}</h3>
-        <h3>Brand: {{ keg.brand }}</h3>
-        <h3>Price: {{ keg.price }}</h3>
-        <h3>Alcohol Content: {{ keg.alcohol }}</h3>
-        <h3>Pints: {{ keg.pints }}</h3>
-      </div>
-  `
-})
-
-export class KegListComponent {
-  public kegList: Keg[];
-  public onKegSelect: EventEmitter<Keg>;
-  public selectedKeg: Keg;
-  constructor(){
-    this.onKegSelect = new EventEmitter();
-  }
-  kegClicked(clickedKeg: Keg): void {
-    console.log('child', clickedKeg);
-    this.selectedKeg = clickedKeg;
-    this.onKegSelect.emit(clickedKeg);
-    }
-  }
-
 
 
 @Component({
